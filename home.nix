32725257eb1fa7a9e48ea9690999309d57785e95
Daniel Robson <danr.236@gmail.com>
2025-04-28 22:25:55 -0400

    fix: debian installation of dotfiles

--- home.nix
+++ home.nix
@@ -1,15 +1,12 @@
-{ config, pkgs, lib, username ? null, homeDirectory ? null, ... }:
+{ config, pkgs, lib, ... }@args:
 
 let
-  # Use provided username/homeDirectory if specified, otherwise use environment variables
-  user = if username != null then username else builtins.getEnv "USER";
-  home = if homeDirectory != null then homeDirectory else builtins.getEnv "HOME";
+  user = args.username or (builtins.getEnv "USER");
+  home = args.homeDirectory or (builtins.getEnv "HOME");
 in
 {
-  # Import the common home configuration
   imports = [ ./home ];
 
-  # Set the username and home directory
   home.username = user;
   home.homeDirectory = home;
 }
