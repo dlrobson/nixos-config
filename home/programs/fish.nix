03785b41456c21eb2ea5180b3c393f50e12ff100
Daniel Robson <danr.236@gmail.com>
2025-04-29 21:40:31 -0400

    fix: fix the clean branches function

--- home/programs/fish.nix
+++ home/programs/fish.nix
@@ -10,7 +10,14 @@
 
     functions = {
       clean_branches = ''
-        git fetch --all --prune && git branch -D (git branch -vv | string match -r ': gone]' | string match -rv '\*' | awk '{ print $1; }')
+        git fetch --all --prune
+        set branches_to_delete (git for-each-ref --format='%(if:equals=[gone])%(upstream:track)%(then)%(refname:short)%(end)' refs/heads/ | string match -v ''')
+
+        if test (count $branches_to_delete) -gt 0
+          git branch -D $branches_to_delete
+        else
+          echo "No stale branches to clean up"
+        end
       '';
     };
   };
