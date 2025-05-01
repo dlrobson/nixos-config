4cd7eb1d4a190c3a36920051875498b894d619e8
Daniel Robson <danr.236@gmail.com>
2025-05-01 10:29:34 -0400

    fix: show applications on debian

--- home.nix
+++ home.nix
@@ -1,10 +1,18 @@
 { config, pkgs, lib, ... }:
 
-{
+let
+  homeDirectory = builtins.getEnv "HOME";
+  username = builtins.getEnv "USER";
+in {
+  # This displays home-manager applications in the system tray
+  targets.genericLinux.enable = true;
+  xdg.mime.enable = true;
+  xdg.systemDirs.data = [ "${homeDirectory}/.nix-profile/share/applications" ];
+
   imports = [
     (import ./home {
-      username = builtins.getEnv "USER";
-      homeDirectory = builtins.getEnv "HOME";
+      homeDirectory = homeDirectory;
+      username = username;
       inherit config pkgs lib;
     })
   ];
