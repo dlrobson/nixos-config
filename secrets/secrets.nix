e52fe5bce7bb07595b76ec9db5789341a857039f
Daniel Robson <danr.236@gmail.com>
2025-05-04 20:53:43 -0400

    chore: add server key to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,10 @@
 let
   laptop =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIc+tZ6XSUqF/7g4IPQXWojEYfa2VI92MrZol7UZV4jd";
+  server =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7H8YJAK3gp/EcDyZKvYbuvT4xFHt8S0lgucTVWKv4W";
 in {
-  "restic/env.age".publicKeys = [ laptop ];
-  "restic/bucket.age".publicKeys = [ laptop ];
-  "restic/password.age".publicKeys = [ laptop ];
+  "restic/env.age".publicKeys = [ laptop server ];
+  "restic/bucket.age".publicKeys = [ laptop server ];
+  "restic/password.age".publicKeys = [ laptop server ];
 }
