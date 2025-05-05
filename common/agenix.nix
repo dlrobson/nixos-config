d8eb8ab0894738059134e3f8097045f4d4e6c536
Daniel Robson <danr.236@gmail.com>
2025-05-04 21:05:46 -0400

    chore: move agenix to common file

---
+++ common/agenix.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+let
+  agenixRepo = builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz";
+in {
+  imports = [
+    "${agenixRepo}/modules/age.nix"
+  ];
+
+  environment.systemPackages = [
+    (pkgs.callPackage "${agenixRepo}/pkgs/agenix.nix" { })
+  ];
+}
--- common/default.nix
+++ common/default.nix
@@ -1,8 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [ ./desktop.nix ./docker.nix ./system.nix ];
+  imports = [ ./agenix.nix ./desktop.nix ./docker.nix ./system.nix ];
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 }
