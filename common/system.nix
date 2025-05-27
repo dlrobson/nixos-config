1014e51f917d97f603211a1d73781ee1a0e55982
Daniel Robson <danr.236@gmail.com>
2025-05-27 19:41:52 -0400

    chore: upgrade home-manager to 25.05

--- common/system.nix
+++ common/system.nix
@@ -5,7 +5,7 @@
     "${
       builtins.fetchTarball
       # TODO(dan): Update this to 25.05 once working.
-      "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz"
+      "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz"
     }/nixos"
   ];
 
