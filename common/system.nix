d04331507c309978eff7543cf47f32297cede31b
Daniel Robson <danr.236@gmail.com>
2025-05-27 19:45:43 -0400

    chore: renmove comment

--- common/system.nix
+++ common/system.nix
@@ -4,7 +4,6 @@
   imports = [
     "${
       builtins.fetchTarball
-      # TODO(dan): Update this to 25.05 once working.
       "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz"
     }/nixos"
   ];
