3fed4db762ef815be4a773ad8d269e6f961e097e
Daniel Robson <danr.236@gmail.com>
2025-05-27 19:21:42 -0400

    chore: todo comment

--- common/system.nix
+++ common/system.nix
@@ -4,6 +4,7 @@
   imports = [
     "${
       builtins.fetchTarball
+      # TODO(dan): Update this to 25.05 once working.
       "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz"
     }/nixos"
   ];
