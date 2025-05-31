5612fb67d51e4c9fbf6c1e29f73ed582db56143d
Daniel Robson <danr.236@gmail.com>
2025-05-31 17:58:02 +0000

    chore: switch identity for server

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   laptop =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIc+tZ6XSUqF/7g4IPQXWojEYfa2VI92MrZol7UZV4jd";
   server =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7H8YJAK3gp/EcDyZKvYbuvT4xFHt8S0lgucTVWKv4W";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBE5GEja6S97XJr9QGOZkXlq5SJXB2Rx8r//hc1zcWnI";
 in {
   "restic/env.age".publicKeys = [ laptop server ];
   "restic/bucket.age".publicKeys = [ laptop server ];
