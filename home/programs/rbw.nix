9a42ccb81eca6605717b24b453aa1460387c35c6
Daniel Robson <danr.236@gmail.com>
2025-05-31 19:30:53 +0000

    chore: remove nixpkgs overlay warning

--- home/programs/rbw.nix
+++ home/programs/rbw.nix
@@ -5,7 +5,7 @@
 
   programs.rbw = {
     enable = true;
-    package = pkgs.unstable.rbw;
+    package = config.unstablePkgs.rbw;
     settings = {
       email = "danr.236@gmail.com";
       pinentry = pkgs.pinentry-gnome3;
