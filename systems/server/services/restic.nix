cbf8e23cc6882ee962ec133560873f7dff63e2a3
Daniel Robson <43209161+dlrobson@users.noreply.github.com>
2025-06-06 22:09:53 -0400

    chore: 2 backups (#65)

--- systems/server/services/restic.nix
+++ systems/server/services/restic.nix
@@ -1,23 +1,26 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  # Common backup configuration
+  commonBackupConfig = {
+    initialize = true;
+    environmentFile = config.age.secrets."restic/env".path;
+    repositoryFile = config.age.secrets."restic/bucket".path;
+    passwordFile = config.age.secrets."restic/password".path;
+    pruneOpts = [ "--keep-daily 7" "--keep-weekly 5" "--keep-monthly 12" ];
+  };
+in {
   # TODO(dan): Notification on backup failure?
   services.restic.backups = {
-    daily = {
-      initialize = true;
+    data = commonBackupConfig // { paths = [ "/mnt/storage/data" ]; };
 
-      environmentFile = config.age.secrets."restic/env".path;
-      repositoryFile = config.age.secrets."restic/bucket".path;
-      passwordFile = config.age.secrets."restic/password".path;
-
-      paths = [ "/mnt/storage/data" "${config.users.users.admin.home}" ];
+    home = commonBackupConfig // {
+      paths = [ "${config.users.users.admin.home}" ];
       exclude = [
         ".git"
         "target"
         "${config.users.users.admin.home}/.local/share/docker"
       ];
-
-      pruneOpts = [ "--keep-daily 7" "--keep-weekly 5" "--keep-monthly 12" ];
     };
   };
 }
