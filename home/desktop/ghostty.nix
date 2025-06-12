93cab4b68d67bbe8779df89d6c476572ac225d93
Daniel Robson <danr.236@gmail.com>
2025-06-12 14:32:43 -0400

    chore: light and dark themes for ghostty

--- home/desktop/ghostty.nix
+++ home/desktop/ghostty.nix
@@ -4,8 +4,12 @@ let isNixOS = builtins.pathExists "/etc/nixos";
 in {
   imports = [ ../../modules/common/unstable-pkgs.nix ./nixgl-pkgs.nix ];
 
-  programs.ghostty.package = if isNixOS then
-    config.unstablePkgs.ghostty
-  else
-    config.lib.nixGL.wrap config.unstablePkgs.ghostty;
+  programs.ghostty = {
+    package = if isNixOS then
+      config.unstablePkgs.ghostty
+    else
+      config.lib.nixGL.wrap config.unstablePkgs.ghostty;
+
+    settings = { theme = "dark:catppuccin-frappe,light:catppuccin-latte"; };
+  };
 }
