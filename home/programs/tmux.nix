13c2da5d0cfbbcce7f43166c9c89c71bf3b6810a
Daniel Robson <danr.236@gmail.com>
2025-06-12 19:39:00 +0000

    chore: tmux heme

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -8,6 +8,8 @@
     terminal = "screen-256color";
 
     plugins = with pkgs.tmuxPlugins; [
+      cpu
+      battery
       {
         plugin = yank;
         extraConfig = "set -g @yank_selection_mouse 'clipboard'";
@@ -23,6 +25,22 @@
       {
         plugin = gruvbox;
         extraConfig = "set -g @tmux-gruvbox 'dark'";
+      }
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor "mocha"
+          set -g @catppuccin_window_status_style "rounded"
+
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -agF status-right "#{E:@catppuccin_status_cpu}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+          set -agF status-right "#{E:@catppuccin_status_battery}"
+        '';
       }
     ];
   };
